25f5ce238b8288763041e60199a8a9c777d5b442
DianeU <diane.uschner@gmail.com>
2020-06-03 15:04:10 -0400

    Added files from the 2014 version of infeR

---
+++ R/infeR-pckg.R
@@ -0,0 +1,24 @@
+#' Randomization based inference for clinical trials
+#'
+#' \tabular{ll}{
+#' Package: \tab infeR \cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0\cr
+#' Date: \tab 2020-06-03\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes \cr
+#' }
+#'
+#' This package provides functionality for randomization based inference in clinical trials
+#'
+#'
+#' @docType package
+#' @name infeR-package
+#' @aliases infeR
+#' @title Randomization based inference for clinical trials
+#' @author Diane Uschner \email{diane.uschner@@gmail.com}
+#' @references Conditional randomization tests in clinical trials
+#' @import randomizeR
+#' @import methods
+#' @import snow
+NULL
